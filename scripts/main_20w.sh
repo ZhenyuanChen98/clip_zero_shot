f73132649701d9c6577f9967b6b917e0956a64e9
Zhenyuan Chen <57855346+Rose-41@users.noreply.github.com>
2023-08-24 17:12:22 +0800

    Add files via upload

---
+++ scripts/main_20w.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# custom config
+DATA=/data/clip_zero_shot/
+TRAINER=Caption_distill_double
+
+DATASET=pazhou_distill_chatglm
+CFG=rn50  # config file
+CTP=end # class token position (end or middle)
+NCTX=16  # number of context tokens
+CSC=False  # class-specific context (False or True)
+run_ID=baseline_20w
+
+
+export CUDA_VISIBLE_DEVICES='0'
+
+### 训练 & 测试
+for SEED in 1 # 2 3
+do
+    DIR=output/${run_ID}/${TRAINER}/${CFG}/nctx${NCTX}_csc${CSC}_ctp${CTP}/seed${SEED}
+    echo "Run this job andsave the output to ${DIR}"
+    python train_caption.py \
+    --root ${DATA} \
+    --seed ${SEED} \
+    --trainer ${TRAINER} \
+    --dataset-config-file configs/datasets/${DATASET}.yaml \
+    --config-file configs/trainers/${TRAINER}/${CFG}.yaml \
+    --output-dir ${DIR} \
+    --datasize '20w' \
+    TRAINER.Caption.N_CTX ${NCTX} \
+    TRAINER.Caption.CSC ${CSC} \
+    TRAINER.Caption.CLASS_TOKEN_POSITION ${CTP} # \
+done
+
+### 从现有的训练模型运行测试
+# for SEED in 1
+# do
+#     DIR=output/evaluation/${run_ID}/${TRAINER}/${CFG}/nctx${NCTX}_csc${CSC}_ctp${CTP}/seed${SEED}
+#     echo "Run this job andsave the output to ${DIR}"
+#     python train_caption.py \
+#     --root ${DATA} \
+#     --seed ${SEED} \
+#     --trainer ${TRAINER} \
+#     --dataset-config-file configs/datasets/${DATASET}.yaml \
+#     --config-file configs/trainers/${TRAINER}/${CFG}.yaml \
+#     --output-dir ${DIR}\
+#     --model-dir output/${run_ID}/${TRAINER}/${CFG}/nctx${NCTX}_csc${CSC}_ctp${CTP}/seed${SEED}\
+#     --eval-only \
+#     TRAINER.Caption.N_CTX ${NCTX} \
+#     TRAINER.Caption.CSC ${CSC} \
+#     TRAINER.Caption.CLASS_TOKEN_POSITION ${CTP}
+#     # fi
+# done
+
+
+
+### chatglm gen
+# bash main.sh pazhou_distill_chatglm rn50 end 16 False pazhou_chatglm_valid 0
---
+++ scripts/others/main_10w_coco.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# custom config
+DATA=/data/clip_zero_shot/
+TRAINER=Caption_distill_double
+
+DATASET=COCO2014_distill
+CFG=rn50_coco  # config file
+CTP=end # class token position (end or middle)
+NCTX=16  # number of context tokens
+CSC=True  # class-specific context (False or True)
+run_ID=coco_chatglm_valid_10w
+
+
+export CUDA_VISIBLE_DEVICES='0,1,2,3,4,5,6,7'
+
+### 训练 & 测试
+for SEED in 1 # 2 3
+do
+    DIR=output/${run_ID}/${TRAINER}/${CFG}/nctx${NCTX}_csc${CSC}_ctp${CTP}/seed${SEED}
+    echo "Run this job andsave the output to ${DIR}"
+    python train_caption.py \
+    --root ${DATA} \
+    --seed ${SEED} \
+    --trainer ${TRAINER} \
+    --dataset-config-file configs/datasets/${DATASET}.yaml \
+    --config-file configs/trainers/${TRAINER}/${CFG}.yaml \
+    --output-dir ${DIR} \
+    TRAINER.Caption.N_CTX ${NCTX} \
+    TRAINER.Caption.CSC ${CSC} \
+    TRAINER.Caption.CLASS_TOKEN_POSITION ${CTP} # \
+done
+
+### 从现有的训练模型运行测试
+# for SEED in 1
+# do
+#     DIR=output/evaluation/${run_ID}/${TRAINER}/${CFG}/nctx${NCTX}_csc${CSC}_ctp${CTP}/seed${SEED}
+#     echo "Run this job andsave the output to ${DIR}"
+#     python train_caption.py \
+#     --root ${DATA} \
+#     --seed ${SEED} \
+#     --trainer ${TRAINER} \
+#     --dataset-config-file configs/datasets/${DATASET}.yaml \
+#     --config-file configs/trainers/${TRAINER}/${CFG}.yaml \
+#     --output-dir ${DIR}\
+#     --model-dir output/${run_ID}/${TRAINER}/${CFG}/nctx${NCTX}_csc${CSC}_ctp${CTP}/seed${SEED}\
+#     --eval-only \
+#     TRAINER.Caption.N_CTX ${NCTX} \
+#     TRAINER.Caption.CSC ${CSC} \
+#     TRAINER.Caption.CLASS_TOKEN_POSITION ${CTP}
+#     # fi
+# done
+
+
+
+### chatglm gen
+# bash main.sh pazhou_distill_chatglm rn50 end 16 False pazhou_chatglm_valid 0
