92f6eb4103228009fe5e63e5e37f35ba4e385efa
Zhenyuan Chen <57855346+Rose-41@users.noreply.github.com>
2023-08-24 17:13:00 +0800

    Add files via upload

---
+++ utils/evaluation.py
@@ -0,0 +1,299 @@
+import math
+import numpy as np
+import torch
+from sklearn.metrics import confusion_matrix
+from collections import OrderedDict
+
+def load_pretrain_weight(state_dict_load):  #去除由于DDP产生的 module 层
+    new_state_dict = OrderedDict()
+    for k, v in state_dict_load.items():
+        namekey = k[7:] if k.startswith('module.') else k
+        new_state_dict[namekey] = v
+
+class AveragePrecisionMeter(object):
+    """
+    The APMeter measures the average precision per class.
+    The APMeter is designed to operate on `NxK` Tensors `output` and
+    `target`, and optionally a `Nx1` Tensor weight where (1) the `output`
+    contains model output scores for `N` examples and `K` classes that ought to
+    be higher when the model is more convinced that the example should be
+    positively labeled, and smaller when the model believes the example should
+    be negatively labeled (for instance, the output of a sigmoid function); (2)
+    the `target` contains only values 0 (for negative examples) and 1
+    (for positive examples); and (3) the `weight` ( > 0) represents weight for
+    each sample.
+    """
+
+    def __init__(self, difficult_examples=False):
+        super(AveragePrecisionMeter, self).__init__()
+        self.reset()
+        self.difficult_examples = difficult_examples
+
+    def reset(self):
+        """Resets the meter with empty member variables"""
+        self.scores = torch.FloatTensor(torch.FloatStorage())
+        self.targets = torch.LongTensor(torch.LongStorage())
+
+
+    def add(self, output, target):
+        """
+        Args:
+            output (Tensor): NxK tensor that for each of the N examples
+                indicates the probability of the example belonging to each of
+                the K classes, according to the model. The probabilities should
+                sum to one over all classes
+            target (Tensor): binary NxK tensort that encodes which of the K
+                classes are associated with the N-th input
+                    (eg: a row [0, 1, 0, 1] indicates that the example is
+                         associated with classes 2 and 4)
+            weight (optional, Tensor): Nx1 tensor representing the weight for
+                each example (each weight > 0)
+        """
+        output = check_tensor(output)
+        target = check_tensor(target)
+
+        if output.dim() == 1:
+            output = output.view(-1, 1)
+        else:
+            assert output.dim() == 2, \
+                'wrong output size (should be 1D or 2D with one column \
+                per class)'
+        if target.dim() == 1:
+            target = target.view(-1, 1)
+        else:
+            assert target.dim() == 2, \
+                'wrong target size (should be 1D or 2D with one column \
+                per class)'
+        if self.scores.numel() > 0:
+            assert target.size(1) == self.targets.size(1), \
+                'dimensions for output should match previously added examples.'
+
+        # make sure storage is of sufficient size
+        if self.scores.storage().size() < self.scores.numel() + output.numel():
+            new_size = math.ceil(self.scores.storage().size() * 1.5)
+            self.scores.storage().resize_(int(new_size + output.numel()))
+            self.targets.storage().resize_(int(new_size + output.numel()))
+
+        # store scores and targets
+        offset = self.scores.size(0) if self.scores.dim() > 0 else 0
+        self.scores.resize_(offset + output.size(0), output.size(1))
+        self.targets.resize_(offset + target.size(0), target.size(1))
+        self.scores.narrow(0, offset, output.size(0)).copy_(output)
+        self.targets.narrow(0, offset, target.size(0)).copy_(target)
+
+
+    def value(self):
+        """Returns the model's average precision for each class
+        Return:
+            ap (FloatTensor): 1xK tensor, with avg precision for each class k
+        """
+
+        if self.scores.numel() == 0:
+            return 0
+        ap = torch.zeros(self.scores.size(1))
+        rg = torch.arange(1, self.scores.size(0)).float()
+        # compute average precision for each class
+        for k in range(self.scores.size(1)):
+            # sort scores
+            scores = self.scores[:, k]
+            targets = self.targets[:, k]
+            # compute average precision
+            ap[k] = AveragePrecisionMeter.average_precision(scores, targets, self.difficult_examples)
+        return ap*100
+
+    @staticmethod
+    def average_precision(output, target, difficult_examples=False):
+
+        # sort examples
+        sorted, indices = torch.sort(output, dim=0, descending=True)
+
+        # Computes prec@i
+        pos_count = 0.
+        total_count = 0.
+        precision_at_i = 0.
+        for i in indices:
+            label = target[i]
+            if difficult_examples and label == 0:
+                continue
+            if label == 1:
+                pos_count += 1
+            total_count += 1
+            if label == 1:
+                precision_at_i += pos_count / total_count
+
+        precision_at_i /= pos_count if pos_count != 0 else -1
+        return precision_at_i
+
+    def overall(self):
+        if self.scores.numel() == 0:
+            return 0
+        scores = self.scores.cpu().numpy()
+        targets = self.targets.cpu().numpy()
+        targets[targets == -1] = 0
+        return self.evaluation(scores, targets)
+
+    def overall_topk(self, k):
+        targets = self.targets.cpu().numpy()
+        targets[targets == -1] = 0
+        n, c = self.scores.size()
+        scores = np.zeros((n, c)) - 1
+        index = self.scores.topk(k, 1, True, True)[1].cpu().numpy()
+        tmp = self.scores.cpu().numpy()
+        for i in range(n):
+            for ind in index[i]:
+                scores[i, ind] = 1 if tmp[i, ind] >= 0 else -1
+        return self.evaluation(scores, targets)
+
+    def evaluation(self, scores_, targets_):
+        n, n_class = scores_.shape
+        Nc, Np, Ng = np.zeros(n_class), np.zeros(n_class), np.zeros(n_class)
+        for k in range(n_class):
+            scores = scores_[:, k]
+            targets = targets_[:, k]
+            targets[targets == -1] = 0
+            Ng[k] = np.sum(targets == 1)
+            Np[k] = np.sum(scores >= 0)
+            Nc[k] = np.sum(targets * (scores >= 0))
+        Np[Np == 0] = 1
+        OP = np.sum(Nc) / np.sum(Np)
+        OR = np.sum(Nc) / np.sum(Ng)
+        OF1 = (2 * OP * OR) / (OP + OR)
+
+        CP = np.sum(Nc / Np) / n_class
+        CR = np.sum(Nc / Ng) / n_class
+        CF1 = (2 * CP * CR) / (CP + CR)
+        return OP*100, OR*100, OF1*100, CP*100, CR*100, CF1*100
+
+
+def check_tensor(tensor):
+    if not torch.is_tensor(tensor):
+        tensor = torch.tensor(tensor)
+    if tensor.device != torch.device('cpu'):
+        tensor = tensor.cpu()
+    return tensor
+
+class Cls_Accuracy():
+    def __init__(self, ):
+        self.total = 0
+        self.correct = 0
+
+    def update(self, logit, label):
+        logit = logit.sigmoid_()
+        logit = (logit >= 0.5)
+        all_correct = torch.all(logit == label.byte(), dim=1).float().sum().item()
+
+        self.total += logit.size(0)
+        self.correct += all_correct
+
+    def compute_avg_acc(self):
+        return self.correct / self.total
+
+
+class RunningConfusionMatrix():
+    """Running Confusion Matrix class that enables computation of confusion matrix
+    on the go and has methods to compute such accuracy metrics as Mean Intersection over
+    Union MIOU.
+
+    Attributes
+    ----------
+    labels : list[int]
+        List that contains int values that represent classes.
+    overall_confusion_matrix : sklean.confusion_matrix object
+        Container of the sum of all confusion matrices. Used to compute MIOU at the end.
+    ignore_label : int
+        A label representing parts that should be ignored during
+        computation of metrics
+
+    """
+
+    def __init__(self, labels, ignore_label=255):
+
+        self.labels = labels
+        self.ignore_label = ignore_label
+        self.overall_confusion_matrix = None
+
+    def update_matrix(self, ground_truth, prediction):
+        """Updates overall confusion matrix statistics.
+        If you are working with 2D data, just .flatten() it before running this
+        function.
+        Parameters
+        ----------
+        groundtruth : array, shape = [n_samples]
+            An array with groundtruth values
+        prediction : array, shape = [n_samples]
+            An array with predictions
+        """
+
+        # Mask-out value is ignored by default in the sklearn
+        # read sources to see how that was handled
+        # But sometimes all the elements in the groundtruth can
+        # be equal to ignore value which will cause the crush
+        # of scikit_learn.confusion_matrix(), this is why we check it here
+        if (ground_truth == self.ignore_label).all():
+            return
+
+        current_confusion_matrix = confusion_matrix(y_true=ground_truth,
+                                                    y_pred=prediction,
+                                                    labels=self.labels)
+
+        if self.overall_confusion_matrix is not None:
+
+            self.overall_confusion_matrix += current_confusion_matrix
+        else:
+
+            self.overall_confusion_matrix = current_confusion_matrix
+
+    def compute_current_mean_intersection_over_union(self):
+
+        intersection = np.diag(self.overall_confusion_matrix)
+        ground_truth_set = self.overall_confusion_matrix.sum(axis=1)
+        predicted_set = self.overall_confusion_matrix.sum(axis=0)
+        union = ground_truth_set + predicted_set - intersection
+
+        # intersection_over_union = intersection / (union.astype(np.float32) + 1e-4)
+        intersection_over_union = intersection / union.astype(np.float32)
+
+        mean_intersection_over_union = np.mean(intersection_over_union)
+
+        return mean_intersection_over_union
+
+
+class IOUMetric:
+    """
+    Class to calculate mean-iou using fast_hist method
+    """
+
+    def __init__(self, num_classes):
+        self.num_classes = num_classes
+        self.hist = np.zeros((num_classes, num_classes))
+
+    def _fast_hist(self, label_pred, label_true):
+        mask = (label_true >= 0) & (label_true < self.num_classes)
+
+        hist = np.bincount(
+            self.num_classes * label_true[mask] + label_pred[mask],
+            minlength=self.num_classes ** 2).reshape(self.num_classes, self.num_classes)
+
+        return hist
+
+    def add_batch(self, predictions, gts):
+        for lp, lt in zip(predictions, gts):
+            self.hist += self._fast_hist(lp.flatten(), lt.flatten())
+
+    def evaluate(self):
+        acc = np.diag(self.hist).sum() / self.hist.sum()
+        acc_cls = np.diag(self.hist) / self.hist.sum(axis=1)
+        acc_cls = np.nanmean(acc_cls)
+        iu = np.diag(self.hist) / (self.hist.sum(axis=1) + self.hist.sum(axis=0) - np.diag(self.hist))
+        mean_iu = np.nanmean(iu)
+        freq = self.hist.sum(axis=1) / self.hist.sum()
+        fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
+        cls_iu = dict(zip(range(self.num_classes), iu))
+
+        return {
+            "Pixel_Accuracy": acc,
+            "Mean_Accuracy": acc_cls,
+            "Frequency_Weighted_IoU": fwavacc,
+            "Mean_IoU": mean_iu,
+            "Class_IoU": cls_iu,
+        }
